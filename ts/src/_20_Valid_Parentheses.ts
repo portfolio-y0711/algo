9f43aac5e96f0b92d08cdf69e46ec3d4d8c709a4
luke <portfolio.y0711@gmail.com>
2022-03-21 20:31:58 +0900

    revise 20

--- ts/src/_20_Valid_Parentheses.ts
+++ ts/src/_20_Valid_Parentheses.ts
@@ -2,19 +2,15 @@ type Parentheses = '(' | ')' | '[' | ']' | '{' | '}'
 
 function isValid(s: string) : boolean {
     const chArr = s.split('') as Parentheses[]
-    if (chArr.length === 1) return false
-    let ch: Parentheses | undefined
+    const len = chArr.length
     const stack: Parentheses[] = []
-    while((ch = chArr.shift()) !== undefined) {
+    for (let i = 0; i <= len; i++) {
+        const ch = chArr.shift()
         switch(ch) {
             case '(':
-                stack.push('(')
-                break
             case '[':
-                stack.push('[')
-                break
             case '{':
-                stack.push('{')
+                stack.push(ch)
                 break
             case ']':
                 if (stack.pop() !== '[') return false
