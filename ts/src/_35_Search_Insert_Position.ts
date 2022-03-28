0a5a83392055bb7d59c74db39a40a4890cf03430
luke <portfolio.y0711@gmail.com>
2022-03-28 22:30:49 +0900

    fix 35

--- ts/src/_35_Search_Insert_Position.ts
+++ ts/src/_35_Search_Insert_Position.ts
@@ -1,18 +1,10 @@
 function searchInsert(nums: number[], target: number): number {
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
+        if (target <= nums[i]) {
             return i
-        } else if (target > nums[i] && target <= nums[i + 1]){
-            return i + 1
         }
     }
-    if (target < nums[0]) {
-        return 0
-    } else if (target > nums[nums.length -1]) {
-        return nums.length
-    } else {
-        return -1
-    }
+    return nums.length
 }
 
-console.log(searchInsert([1], 1))
+console.log(searchInsert([1,3,5,6], 7))
