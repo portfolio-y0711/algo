69b3db43cd867d744dbe55073a0623146a130365
luke <portfolio.y0711@gmail.com>
2022-03-31 22:03:28 +0900

    fix 58

--- ts/src/_58_Length_of_Last_Word.ts
+++ ts/src/_58_Length_of_Last_Word.ts
@@ -1,10 +1,9 @@
 function lengthOfLastWord(s: string) {
     const target = s.trim()
-    let result: number | undefined
     for (let i = 0; i < target.length -1; i++) {
         if (target[target.length -1 - i] === ' ') {
             return i
         }
     }
-    return result ?? target.length
+    return target.length
 }
