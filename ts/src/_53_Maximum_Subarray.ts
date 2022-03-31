6809a71e5087fed42b8e3990ba86050872d25fb1
luke <portfolio.y0711@gmail.com>
2022-03-31 22:02:54 +0900

    fix 53

--- ts/src/_53_Maximum_Subarray.ts
+++ ts/src/_53_Maximum_Subarray.ts
@@ -1,18 +1,18 @@
 function maxSubArray(nums: number[]): number {
-    let result = -10000
+    let result = - Infinity
     let sum = 0
   
     for (let i = 0; i < nums.length; i++) {
       sum += nums[i]
   
       if (sum > result) {
-        result = sum;
+        result = sum
       }
   
       if (sum < 0) {
         sum = 0
       }
     }
-    return result;
+    return result
 }
 
