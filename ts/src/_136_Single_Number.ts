4d870c8425ce4d8e317683cc757daaf6f84a7704
luke <portfolio.y0711@gmail.com>
2022-05-23 09:55:58 +0900

    add singleNumber

---
+++ ts/src/_136_Single_Number.ts
@@ -0,0 +1,8 @@
+function singleNumber(nums: number[]): number | undefined {
+    const numCountMap: Record<number, number> = {}
+    for(let i = 0; i < nums.length; i++) {
+        const count = numCountMap[nums[i]] ? numCountMap[nums[i]] : 0
+        numCountMap[nums[i]] = count + 1
+    }
+    return Object.keys(numCountMap).filter((key) => numCountMap[key as any]) as unknown as number
+}
