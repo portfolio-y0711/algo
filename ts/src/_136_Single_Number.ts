f86a2ce79ff9a237d195f507af1e64518485a65f
luke <portfolio.y0711@gmail.com>
2022-05-23 23:13:58 +0900

    fix single number

--- ts/src/_136_Single_Number.ts
+++ ts/src/_136_Single_Number.ts
@@ -4,5 +4,5 @@ function singleNumber(nums: number[]): number | undefined {
         const count = numCountMap[nums[i]] ? numCountMap[nums[i]] : 0
         numCountMap[nums[i]] = count + 1
     }
-    return Object.keys(numCountMap).filter((key) => numCountMap[key as any]) as unknown as number
+    return Object.keys(numCountMap).filter((key) => numCountMap[key as any] === 1) as unknown as number
 }
